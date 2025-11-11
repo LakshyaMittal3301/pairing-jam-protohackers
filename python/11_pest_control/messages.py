6a7360aea8021ba55cafdbe4228590697bebfbca
alyxya <hi@alyxya.com>
2025-11-11 15:25:54 -0500

    create a separate messages.py

---
+++ python/11_pest_control/messages.py
@@ -0,0 +1,121 @@
+def encode_u32(n: int) -> bytes:
+    assert n >= 0 and n <= (2**32 - 1)
+    return bytes([(n >> 24) % 256, (n >> 16) % 256, (n >> 8) % 256, n % 256])
+
+
+def encode_str(s: str) -> bytes:
+    return encode_u32(len(s)) + s.encode("utf-8")
+
+
+def message_wrapper(message_type: bytes, contents: bytes) -> bytes:
+    assert len(message_type) == 1
+    message_len = len(contents) + 1 + 4 + 1
+    message = message_type + encode_u32(message_len) + contents
+    checksum = 0
+    for val in message:
+        checksum -= val
+    checksum %= 256
+    return message + bytes([checksum])
+
+
+def hello_message(protocol: str, version: int) -> bytes:
+    return message_wrapper(b"\x50", encode_str(protocol) + encode_u32(version))
+
+
+def error_message(message: str) -> bytes:
+    return message_wrapper(b"\x51", encode_str(message))
+
+
+def dial_authority_message(site: int) -> bytes:
+    return message_wrapper(b"\x53", encode_u32(site))
+
+
+def create_policy_message(species: str, action: bytes) -> bytes:
+    assert len(action) == 1
+    assert action == b"\x90" or action == b"\xa0"
+    return message_wrapper(b"\x55", encode_str(species) + action)
+
+
+def delete_policy_message(policy: int) -> bytes:
+    return message_wrapper(b"\x56", encode_u32(policy))
+
+
+def parse_u32(b: bytes, index: int) -> int:
+    # convert 4 byte unsigned integer (u32) to the integer type
+    assert len(b) >= index + 4
+    return (b[index] << 24) + (b[index + 1] << 16) + (b[index + 2] << 8) + b[index + 3]
+
+
+def parse_str(b: bytes, index: int) -> str:
+    str_len = parse_u32(b, index)
+    assert len(b) >= index + 4 + str_len
+    return b[index + 4 : index + 4 + str_len].decode("utf-8")
+
+
+def parse_array(
+    b: bytes, index: int, spec: dict[str, str]
+) -> list[dict[str, int | str]]:
+    arr_len = parse_u32(b, index)
+    index += 4
+    arr = []
+    for _ in range(arr_len):
+        # parse element
+        elem = {}
+        for key, value in spec.items():
+            if value == "u32":
+                # parse u32
+                int_val = parse_u32(b, index)
+                index += 4
+                elem[key] = int_val
+            elif value == "str":
+                # parse str
+                str_val = parse_str(b, index)
+                index += 4 + len(str_val)
+                elem[key] = str_val
+            else:
+                raise Exception(f"Unknown spec type {value}")
+        arr.append(elem)
+    return arr
+
+
+def parse_hello_message(b: bytes) -> dict[str, str | int]:
+    index = 5
+    protocol = parse_str(b, index)
+    index += 4 + len(protocol)
+    version = parse_u32(b, index)
+    index += 4
+    return {"protocol": protocol, "version": version}
+
+
+def parse_error_message(b: bytes) -> dict[str, str]:
+    index = 5
+    message = parse_str(b, index)
+    index += 4 + len(message)
+    return {"message": message}
+
+
+def parse_ok_message(b: bytes) -> dict:
+    return {}
+
+
+def parse_target_populations_message(b: bytes):
+    index = 5
+    site = parse_u32(b, index)
+    index += 4
+    populations = parse_array(b, index, {"species": "str", "min": "u32", "max": "u32"})
+    return {"site": site, "populations": populations}
+
+
+def parse_policy_result_message(b: bytes):
+    index = 5
+    policy = parse_u32(b, index)
+    index += 4
+    return {"policy": policy}
+
+
+def parse_site_visit_message(b: bytes):
+    index = 5
+    site = parse_u32(b, index)
+    index += 4
+    populations = parse_array(b, index, {"species": "str", "count": "u32"})
+    return {"site": site, "populations": populations}
